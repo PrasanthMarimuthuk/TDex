eb9128377e9db55e0dc1db5b5a220daac3124c1a
Sivapprakash Chinnaraj <sivapprakash1634@gmail.com>
2025-02-18 11:16:58 +0530

    main page

--- app/src/main/java/com/example/tdexv01/MapActivity.kt
+++ app/src/main/java/com/example/tdexv01/MapActivity.kt
@@ -7,7 +7,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
+
 
 
 class MapActivity : AppCompatActivity() {
